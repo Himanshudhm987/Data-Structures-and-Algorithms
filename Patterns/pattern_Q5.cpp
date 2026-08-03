646f4f73b203752ab905c2d69785224fa2b41133
Himanshu <himanshudhiman4500@gmail.com>
2026-08-03 16:28:42 +0530

    Add pattern printing solutions

---
+++ Patterns/pattern_Q5.cpp
@@ -0,0 +1,24 @@
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+
+
+
+#include<iostream>
+using namespace std;
+void print(int n){
+    for (int i=0;i<n;i++){
+        for (int j=1;j<=n-i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}              
