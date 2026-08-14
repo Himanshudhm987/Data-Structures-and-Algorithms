6e3e35664413129779aac530eae2587e5ec60604
Himanshu <himanshudhiman4500@gmail.com>
2026-08-14 23:51:14 +0530

    Print n names

---
+++ RECURSION/Print_n_times_using_recursion.cpp
@@ -0,0 +1,21 @@
+// HelloWorld
+// HelloWorld
+// HelloWorld
+// HelloWorld
+// HelloWorld
+
+#include<bits/stdc++.h>
+using namespace std;
+void print(int n,int i){
+    if(i>n){
+        return;
+    }
+    cout<<"HelloWorld"<<endl;
+    print(n,i+1);
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n,1);
+    return 0;
+}
