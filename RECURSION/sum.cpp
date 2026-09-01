6fb5f16ce8f7c2b64601d0893caeecb3829c61a2
Himanshu <himanshudhiman4500@gmail.com>
2026-09-01 19:06:20 +0530

    function added

--- RECURSION/sum.cpp
+++ RECURSION/sum.cpp
@@ -1,12 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-int func(int n){
-    if(n<=0) return 0;
-    return n+func(n-1);
+// int func(int n){
+//     if(n<=0) return 0;
+//     return n+func(n-1);
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     cout<<func(n);
+//     return 0;
+// }
+
+void func(int sum,int n){
+    if(n<=0) {
+    cout<<sum;
+    return ;
+}
+    sum+=n;
+    func(sum,n-1);
 }
 int main(){
-    int n;
+    int n,sum=0;
     cin>>n;
-    cout<<func(n);
+    func(sum,n);
     return 0;
 }
