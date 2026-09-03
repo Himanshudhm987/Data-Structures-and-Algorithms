3eb871c24fac5e3c71710b38a58b53617ee00493
Himanshu <himanshudhiman4500@gmail.com>
2026-09-04 00:58:38 +0530

    Reverse of an array

---
+++ RECURSION/reverse_an_array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverse(int i,int arr[] , int n){
+    if(i>=(int)(n/2)) return;
+    int temp=arr[i];
+    arr[i]=arr[n-i-1];
+    arr[n-i-1]=temp;
+    reverse( i+1, arr ,n);
+}
+void output( int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    reverse(0,arr,n);
+    output(arr,n);
+
+    return 0;
+}
