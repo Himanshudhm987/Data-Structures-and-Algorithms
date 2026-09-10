f91b1bbf96d86d458bdd723bf1bc15713a4b8d91
Himanshu <himanshudhiman4500@gmail.com>
2026-09-10 20:29:34 +0530

    Hashing

---
+++ RECURSION/hashing.cpp
@@ -0,0 +1,23 @@
+// number of times number present in an array 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    // pre compute
+    int hash[13]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]]+=1;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        cout<<hash[num]<<endl;
+    }
+}
