55c42fdc3342ac9055358955311ca7c39ac25004
Himanshu <himanshudhiman4500@gmail.com>
2026-09-18 19:54:48 +0530

    bubble sort added

---
+++ SORTING/bubble_sort.cpp
@@ -0,0 +1,34 @@
+// 8 6 9 4 2
+// 9
+// 8
+// 6
+// 4
+// 2
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=n-1;i>=1;i--){
+        int didswap=0;
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]<arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                didswap=1;
+            }
+        }
+        if(didswap==0){
+                break;
+            }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
