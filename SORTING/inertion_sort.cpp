f7ad96ca6326b4d29aea3c6139cdecb523ec5cf2
Himanshu <himanshudhiman4500@gmail.com>
2026-09-20 08:45:31 +0530

    Insertion sort added

---
+++ SORTING/inertion_sort.cpp
@@ -0,0 +1,32 @@
+// 5
+// 8 6 9 4 2
+
+// 2
+// 4
+// 6
+// 8
+// 9
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        int j=i;
+        while((j>0)&&(arr[j-1]>arr[j])){
+            int temp =arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=temp;
+            j--;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
